73110034cb0fde54d7bb05b7cee5d485b8ec65ea
Adrian-YongKeng <141707001+Adrian-YongKeng@users.noreply.github.com>
2023-10-09 13:10:41 +0000

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,67 @@
+import product1 from "./assets/lebronxx-ep.jpeg";
+import product2 from "./assets/kobe5rings.jpeg";
+import product3 from "./assets/kobepop.jpeg";
+import product4 from "./assets/lebron23.avif";
+import product5 from "./assets/lebron233.avif";
+import product6 from "./assets/jordan23.webp";
+import product7 from "./assets/lebronxx-ep.jpeg";
+import product8 from "./assets/kobe5rings.jpeg";
+import product9 from "./assets/kobepop.jpeg";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Nike Lebron Xx Ep Men's Basketball Shoes - Black -",
+    price: 300.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Nike Kobe 5 Protro '5 Rings'",
+    price: 349.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Nike Kobe 6 Protro 'Pop'",
+    price: 389.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "LeBron James Jersey",
+    price: 130.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LeBron James Jersey - White -",
+    price: 120.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Michael Jordan Jersey",
+    price: 300.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Nike Lebron Xx Ep Men's Basketball Shoes - Black -",
+    price: 300.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Nike Kobe 5 Protro '5 Rings'",
+    price: 349.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Nike Kobe 6 Protro 'Pop'",
+    price: 389.0,
+    productImage: product9,
+  },
+];
